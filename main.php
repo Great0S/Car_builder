9816bf97e9a2742647854a84e8eebd635bd8e92e
Great0s <greatos@outlook.com>
2023-01-15 22:52:46 +0300

    first commit

---
+++ main.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Plugin Name:  Car Builder
+ * Plugin URI:   https://www.fasthosts.co.uk
+ * Description:  Fasthosts Assistant will help you complete the first setup of your WordPress in quick and easy steps. It will help you find a theme to start with and add some plugins that will help you with the purpose of your WordPress installation.
+ * Version:      1.0
+ * License:      GPL-2.0-or-later
+ * Author:       GreatOS
+ * Author URI:   https://www.fasthosts.co.uk
+ * Text Domain:  fasthosts-assistant
+ * Domain Path:  /languages
+ */
+
+/*
+Copyright 2020 Fasthosts by 1&1
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Online: http://www.gnu.org/licenses/gpl.txt
+*/
+
+
+
+
+?>
