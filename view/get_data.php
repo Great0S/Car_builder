68f9a8224f54312657bde5d6818bcd3cf52c7611
Great0s <greatos@outlook.com>
2023-01-27 01:16:27 +0300

    added input validation

--- view/get_data.php
+++ view/get_data.php
@@ -14,15 +14,23 @@ require(plugin_dir_path(__FILE__) . '..\includes\data.php');
 $_SESSION['times']++;
 
 
-if ((int) $_POST['id'] == 1) {
-    $_SESSION['brand'] = $selected_item = $_POST['value'];
-    $selected_model = "";
-} else if ((int) $_POST['id'] == 2) {
-    $_SESSION['model'] = $selected_model = $_POST['value'];
-    echo "<script>alert('model is set:" . $selected_model . "')</script>";
-} else {
-    $selected_item = $_POST['value'];
-    echo "<script>alert('item is set:" . $selected_item . "')</script>";
+if (isset($_POST['id']) && is_numeric($_POST['id'])) {
+    $id = (int) isset($_POST['id']) ? htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8') : null;
+    $value = isset($_POST['value']) ? htmlspecialchars($_POST['value'], ENT_QUOTES, 'UTF-8') : null;
+
+    // Check if the value is set
+    if ($id == 1) {
+        $_SESSION['brand'] = $selected_item = $value;
+        $selected_model = "";
+    } else if ($id == 2) {
+        $_SESSION['model'] = $selected_model = $value;
+        echo "<script>alert('model is set:" . $selected_model . "')</script>";
+    } else {
+        $selected_item = $value;
+        echo "<script>alert('item is set:" . $selected_item . "')</script>";
+    }
 }
 
-return process_data($cars_file, $cars_data, $file,  $selected_item,  $selected_brand, $selected_model);
+
+format($_SESSION);
+return process_data($cars_file, $cars_data, $file,  $selected_item);
